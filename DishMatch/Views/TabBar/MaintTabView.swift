74603229d8986d9ee34d3af1fa11c17da6b59fe3
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-22 19:52:23 +0900

    MainTabViewを作成

---
+++ DishMatch/Views/TabBar/MaintTabView.swift
@@ -0,0 +1,31 @@
+//
+//  TabBarView.swift
+//  DishMatch
+//
+//  Created by 大江悠都 on 2025/01/22.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView{
+            Text("CardStackView()")
+                .tabItem { Image (systemName: "fork.knife")}
+                .tag(0)
+            
+            Text("LiksView()")
+                .tabItem { Image (systemName: "heart.fill")}
+                .tag(1)
+            
+            Text("UserProfileView()")
+                .tabItem{ Image (systemName: "person")}
+                .tag(2)
+        }
+        .tint(.primary)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
