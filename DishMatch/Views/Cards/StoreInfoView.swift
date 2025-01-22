6a7e47fc2dae8811d70513d652c228f90051b4aa
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-22 21:05:46 +0900

    StoreInfoに必要なTextとImageを表示

---
+++ DishMatch/Views/Cards/StoreInfoView.swift
@@ -0,0 +1,48 @@
+//
+//  StoreInfoView.swift
+//  DishMatch
+//
+//  Created by 大江悠都 on 2025/01/22.
+//
+
+import SwiftUI
+
+struct StoreInfoView: View {
+    let store: Store
+    var body: some View {
+        VStack (alignment: .leading){
+            HStack {
+                Text(store.storeName)
+                    .font(.title)
+                    .fontWeight(.heavy)
+                
+                Spacer()
+                
+                // プロフィール表示ボタン
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.up.circle")
+                        .fontWeight(.bold)
+                        .imageScale(.large)
+                }
+            }
+            HStack {
+                Image(systemName: "fork.knife")
+                Text("\(store.genre)")
+                    .font(.subheadline)
+                    .lineLimit(2)
+                Text("|")
+                Image(systemName: "mappin.and.ellipse")
+                Text("\(store.station_name)")
+                
+                
+            }
+            
+        }
+    }
+}
+
+#Preview {
+    StoreInfoView(store: MockData.stores[1])
+}
