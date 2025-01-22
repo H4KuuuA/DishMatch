6fc532314f901d0d82733224f4317bbb29e058a3
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-22 22:09:47 +0900

    CardServiceを利用してカードモデルのデータを非同期で取得する機能を追加

--- DishMatch/Model/Cards/CardsViewModel.swift
+++ DishMatch/Model/Cards/CardsViewModel.swift
@@ -9,5 +9,22 @@ import Foundation
 
 @MainActor
 class CardsViewModel: ObservableObject {
-    @Published var cardsModels = [CardModel]()
+    @Published var cardModels = [CardModel]()
+    private let service: CardService
+    
+    init(service: CardService) {
+        self.service = service
+        Task {
+            await fetchCardModels()
+        }
+    }
+    
+    // カードのデータを取得する
+    func fetchCardModels() async {
+        do {
+            self.cardModels = try await service.fetchCardModels()
+        } catch {
+            print("DEBUG: fetchCardModels error \(error)")
+        }
+    }
 }
