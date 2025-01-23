c73c62ab5be4d0b07dc9e50499121c2ed4003876
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 10:58:42 +0900

    ReserveButtonViewに電話ボタンとネットで予約ボタンを追加

---
+++ DishMatch/Views/Profile/ReserveButtonView.swift
@@ -0,0 +1,38 @@
+//
+//  ReserveButtonView.swift
+//  DishMatch
+//
+//  Created by 大江悠都 on 2025/01/23.
+//
+
+import SwiftUI
+
+struct ReserveButtonView: View {
+    var body: some View {
+        HStack {
+            Button(action: {}) {
+                Image(systemName: "phone.fill")
+                    .foregroundColor(.gray .opacity(0.8))
+                    .frame(width: 60, height: 48)
+                    .background(Color.white)
+                    .cornerRadius(24)
+            }
+            Spacer()
+                
+            Button(action: {}) {
+                    Text("ネットで予約")
+                        .padding()
+                        .padding(.horizontal)
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 240, height: 48)
+                        .background(Color.orange)
+                        .cornerRadius(24)
+                }
+        }
+    }
+}
+
+#Preview {
+    ReserveButtonView()
+}
