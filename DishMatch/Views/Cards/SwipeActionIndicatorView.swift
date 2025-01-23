2315f4dc1c7983ca77bf22e2693fd55bf4149ef4
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 09:14:24 +0900

    SwipeActionIndicatorのTextを装飾

--- DishMatch/Views/Cards/SwipeActionIndicatorView.swift
+++ DishMatch/Views/Cards/SwipeActionIndicatorView.swift
@@ -11,10 +11,26 @@ struct SwipeActionIndicatorView: View {
     var body: some View {
         HStack {
             Text("LIKE")
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundStyle(.green)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(Color.green, lineWidth: 2)
+                        .frame(width: 100, height: 48)
+                }
             
             Spacer()
             
             Text("NONE")
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 4)
+                        .stroke(.red, lineWidth: 2)
+                        .frame(width: 100, height: 48)
+                }
         }
         .padding(40)
     }
