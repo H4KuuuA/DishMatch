bc9dd913fb1200236252f4b0b4bbaf88a0edcf86
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 13:00:22 +0900

    LikesViewにエラーメッセージのデモを表示

--- DishMatch/Views/Likes/LikesView.swift
+++ DishMatch/Views/Likes/LikesView.swift
@@ -12,6 +12,18 @@ struct LikesView: View {
         VStack {
             TextFieldLikeView()
             GenreTabBarView(LikesViewModel: LikesViewModel(stores: MockData.stores), isGenreActive: true)
+            
+            ScrollView {
+                VStack (alignment: .center, spacing: 20){
+                    Spacer()
+                    Image(systemName: "exclamationmark.triangle")
+                        .foregroundColor(.gray.opacity(0.5))
+                        .font(.system(size: 30))
+                    Text("店舗情報の取得に失敗しました。再試行してください。")
+                        .foregroundColor(.gray.opacity(0.8))
+                        .padding(.horizontal, 20)
+                }
+            }
         }
     }
 }
