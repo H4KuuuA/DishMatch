05ac7e607bc354750abe5ce1f860ff962ed87284
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 10:23:45 +0900

    BackCardButtonViewに戻るボタン追加

---
+++ DishMatch/Views/Cards/BackCardButtonView.swift
@@ -0,0 +1,32 @@
+//
+//  BackCardButtonView.swift
+//  DishMatch
+//
+//  Created by 大江悠都 on 2025/01/23.
+//
+
+import SwiftUI
+
+struct BackCardButtonView: View {
+    var body: some View {
+        Button {
+            
+        } label: {
+            Image(systemName: "arrow.counterclockwise")
+                .fontWeight(.heavy)
+                .foregroundStyle(.yellow)
+                .background {
+                    Circle()
+                        .fill(.white)
+                        .frame(width: 48, height: 48)
+                        .shadow(radius: 6)
+                }
+                
+        }
+        .frame(width: 48, height: 48)
+    }
+}
+
+#Preview {
+    BackCardButtonView()
+}
