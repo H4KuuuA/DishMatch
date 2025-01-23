bcc5275bf8ffeff1e33da37dee6a5de367a15366
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 11:59:24 +0900

    UserProfileViewに会員登録情報のボタンを追加

--- DishMatch/Views/Profile/UserProfileView.swift
+++ DishMatch/Views/Profile/UserProfileView.swift
@@ -12,6 +12,87 @@ struct UserProfileView: View {
         NavigationStack {
             List {
                 UserProfileHeaderView()
+                
+                Section("会員登録情報") {
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Image(systemName: "person.fill")
+                                .foregroundStyle(.orange)
+                                .padding(.trailing)
+                            Text("登録情報")
+                                .foregroundStyle(.black)
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Image(systemName: "calendar.badge.checkmark")
+                                .foregroundStyle(.orange)
+                                .padding(.trailing)
+                            Text("予約履歴")
+                                .foregroundStyle(.black)
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Image(systemName: "mappin.and.ellipse")
+                                .foregroundStyle(.orange)
+                                .padding(.trailing)
+                            Text("行ったお店")
+                                .foregroundStyle(.black)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Image(systemName: "tag.fill")
+                                .foregroundStyle(.orange)
+                                .padding(.trailing)
+                            Text("クーポン")
+                                .foregroundStyle(.black)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                    Button {
+                        
+                    } label: {
+                        HStack {
+                            Image(systemName: "gearshape.fill")
+                                .foregroundStyle(.orange)
+                                .padding(.trailing)
+                            Text("アプリ設定")
+                                .foregroundStyle(.black)
+                            
+                            Spacer()
+                            
+                            Image(systemName: "chevron.right")
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
             }
         }
     }
