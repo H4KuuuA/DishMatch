b2168e1dc9c955eaa95e11d085eb01672c1e9062
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 10:44:14 +0900

    StoreProfileViewファイルを追加

---
+++ DishMatch/Views/Profile/StoreProfileView.swift
@@ -0,0 +1,18 @@
+//
+//  StoreProfileView.swift
+//  DishMatch
+//
+//  Created by 大江悠都 on 2025/01/23.
+//
+
+import SwiftUI
+
+struct StoreProfileView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StoreProfileView()
+}
