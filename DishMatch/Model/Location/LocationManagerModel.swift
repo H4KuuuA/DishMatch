cc18a4596273e30da63e655bb39cc9b5f86ebcec
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 22:30:56 +0900

    LocationManagerにCLLocationManagerDelegateの実装を追加

--- DishMatch/Model/Location/LocationManagerModel.swift
+++ DishMatch/Model/Location/LocationManagerModel.swift
@@ -21,4 +21,27 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
         // 権限が変更されるのを待ってから位置情報の更新を開始
         locationManager.requestAlwaysAuthorization()
     }
+    
+    // 権限変更時に呼ばれるデリゲートメソッド
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // 権限が許可されている場合にのみ位置情報の更新を開始
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            if CLLocationManager.locationServicesEnabled() {
+                locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+                locationManager.distanceFilter = 10
+                locationManager.allowsBackgroundLocationUpdates = true
+                locationManager.pausesLocationUpdatesAutomatically = false
+                locationManager.startUpdatingLocation()
+            }
+        }
+    }
+    
+    // 位置情報が更新されたときに呼ばれるデリゲートメソッド
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let newLocation = locations.last else { return }
+        DispatchQueue.main.async {
+            self.latitude = newLocation.coordinate.latitude
+            self.longitude = newLocation.coordinate.longitude
+        }
+    }
 }
