a94d148097ba284981e197f8d82e68fee5593910
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 22:28:26 +0900

    LocationManagerの基本構造と初期化

--- DishMatch/Model/Location/LocationManagerModel.swift
+++ DishMatch/Model/Location/LocationManagerModel.swift
@@ -5,26 +5,20 @@
 //  Created by 大江悠都 on 2025/01/23.
 //
 
-import CoreLocation
 import Foundation
+import CoreLocation
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-    private let locationManager = CLLocationManager()
-    @Published var location: CLLocation? = nil
-
+    private var locationManager = CLLocationManager()
+    
+    @Published var latitude: Double = 0.0
+    @Published var longitude: Double = 0.0
+    
     override init() {
         super.init()
-        // CLLocationManagerの位置情報を受け取るために自分に設定
-        self.locationManager.delegate = self
-        // 位置情報の利用をユーザーに要求
-        self.locationManager.requestWhenInUseAuthorization()
-        self.locationManager.startUpdatingLocation()
-    }
-
-    /// 位置情報の更新があった際に呼ばれるデリゲートメソッド
-    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if let location = locations.last {
-            self.location = location
-        }
+        locationManager.delegate = self
+        
+        // 権限が変更されるのを待ってから位置情報の更新を開始
+        locationManager.requestAlwaysAuthorization()
     }
 }
