df84bfe103e1912847cef2450f89817f3774ed8f
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 11:52:14 +0900

    ユーザーアイコンの右上に編集用ボタンを追加

--- DishMatch/Views/Profile/UserProfileHeaderView.swift
+++ DishMatch/Views/Profile/UserProfileHeaderView.swift
@@ -10,18 +10,34 @@ import SwiftUI
 struct UserProfileHeaderView: View {
     var body: some View {
         VStack {
-            Image("UserProfileImage")
-                .resizable()
-                .scaledToFill()
-                .frame(width: 120, height: 120)
-                .clipShape(Circle())
-                .background {
-                    Circle()
-                        .fill(Color(.systemGray6))
-                        .frame(width: 128, height: 128)
-                        .shadow(radius: 10)
+            ZStack (alignment: .topTrailing){
+                Image("UserProfileImage")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 120, height: 120)
+                    .clipShape(Circle())
+                    .background {
+                        Circle()
+                            .fill(Color(.systemGray6))
+                            .frame(width: 128, height: 128)
+                            .shadow(radius: 10)
+                    }
+                
+                Button(action: {}) {
+                    Image(systemName: "pencil")
+                        .imageScale(.small)
+                        .foregroundStyle(.gray)
+                        .background {
+                            Circle()
+                                .fill(.white)
+                                .frame(width: 32, height: 32)
+                        }
+                    
                 }
-                .padding()
+                .offset(x: -8, y: 10)
+                
+            }
+            .padding()
             Text("ご飯探検隊")
                 .font(.title2)
                 .fontWeight(.bold)
