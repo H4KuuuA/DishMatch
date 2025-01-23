f9b9d5630e626ed1dc0bcecd6e05eecd22fb39f5
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-23 12:47:10 +0900

    LikesViewModelに重複するgenreをリストから消去する関数と、更新の管理

---
+++ DishMatch/Model/Cards/LikesViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  LikesViewModel.swift
+//  DishMatch
+//
+//  Created by 大江悠都 on 2025/01/23.
+//
+
+import Foundation
+import Combine
+
+class LikesViewModel: ObservableObject {
+    @Published var uniqueStores: [Store] = []
+
+    private var stores: [Store] = [] {
+        didSet {
+            updateUniqueStores()
+        }
+    }
+
+    init(stores: [Store]) {
+        self.stores = stores
+        updateUniqueStores()
+    }
+
+    // storesが変更された場合にuniqueStoresを更新
+    func updateStores(_ newStores: [Store]) {
+        self.stores = newStores
+    }
+
+    // 重複するジャンルを排除し、最初のStoreを取得
+    private func updateUniqueStores() {
+        let uniqueGenres = Array(Set(stores.map { $0.genre })) // ジャンルの重複を排除
+        uniqueStores = uniqueGenres.compactMap { genre in
+            // genreに一致する最初のStoreを取得
+            stores.first { $0.genre == genre }
+        }
+    }
+}
