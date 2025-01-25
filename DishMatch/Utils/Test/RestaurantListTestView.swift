6131c70893fc7feb2c46d568cd605ec179091abe
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-25 22:42:08 +0900

    APIを正確に受け取れるように問題を解消

--- DishMatch/Utils/Test/RestaurantListTestView.swift
+++ DishMatch/Utils/Test/RestaurantListTestView.swift
@@ -9,23 +9,19 @@ import SwiftUI
 
 struct RestaurantListTestView: View {
     @StateObject private var viewModel = RestaurantViewModel()
-    
-    @State private var restaurants: [Shop] = []
-    @State private var isLoading = true
-    
 
     var body: some View {
         NavigationView {
             VStack {
-                if isLoading {
+                if viewModel.isLoading {
                     ProgressView("データを取得中...") // ローディング表示
                         .padding()
-                } else if restaurants.isEmpty {
+                } else if viewModel.restaurants.isEmpty {
                     Text("データが見つかりませんでした。")
                         .foregroundColor(.gray)
                         .padding()
                 } else {
-                    List(restaurants) { restaurant in
+                    List(viewModel.restaurants) { restaurant in
                         RestaurantRowView(restaurant: restaurant)
                     }
                 }
