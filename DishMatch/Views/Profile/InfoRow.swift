d0e931b67322b45fe2d85f5e191948eaa49e44c6
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-26 21:10:01 +0900

    InfoRowを作成

---
+++ DishMatch/Views/Profile/InfoRow.swift
@@ -0,0 +1,31 @@
+//
+//  StoreProfileDetailView.swift
+//  DishMatch
+//
+//  Created by 大江悠都 on 2025/01/26.
+//
+
+import SwiftUI
+
+struct InfoRow: View {
+    var title: String
+    var detail: String
+    
+    var body: some View {
+        HStack {
+            Text(title)
+                .font(.headline)
+                .foregroundColor(Color("FC"))
+                .frame(width: 80, alignment: .leading)
+            Text(detail)
+                .font(.subheadline) 
+            Spacer()
+        }
+        
+    }
+}
+
+#Preview {
+    InfoRow(title: "住所", detail: "123 Tokyo Street")
+        .padding()
+}
