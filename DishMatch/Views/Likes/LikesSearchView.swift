53564c40b3e175e438d18c6fcac63de88f1a1eb2
H4KuuuA <kd1419898@st.kobedenshi.ac.jp>
2025-01-31 11:15:00 +0900

    LikesSearchViewに検索ようの関数を追加

--- DishMatch/Views/Likes/LikesSearchView.swift
+++ DishMatch/Views/Likes/LikesSearchView.swift
@@ -9,11 +9,24 @@ import SwiftUI
 
 struct LikesSearchView: View {
     @Binding var isPresented: Bool
-    @State private var serachText: String = ""
-    @State private var searcHistory : [String] = []
+    @State private var searchText: String = ""
+    @State private var searchHistory : [String] = []
     @State private var keyboardOffset: CGFloat = 0
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+    /// 検索実行 (仮)
+    private func performSearch() {
+        if !searchText.isEmpty {
+            if !searchHistory.contains(searchText) {
+                searchHistory.insert(searchText, at: 0) // 新しい検索履歴を先頭に追加
+            }
+        }
+    }
+
+    /// 履歴の削除
+    private func removeHistory(at offsets: IndexSet) {
+        searchHistory.remove(atOffsets: offsets)
     }
 }
 
